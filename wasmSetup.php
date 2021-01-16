8abfe7074ab884539ffeacb7d459a0cbcaeef246
wangchao <1613529835@qq.com>
2021-01-16 11:16:17 +0800

    1

---
+++ wasmSetup.php
@@ -0,0 +1,51 @@
+<?php
+
+$dn = pathinfo(__FILE__)["dirname"];
+$dna = explode(DIRECTORY_SEPARATOR, $dn);
+$projectName = $dna[count($dna) - 1];
+
+$indexHtml = <<<HTML
+<html>
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
+</head>
+<body>
+<canvas id="canvas" width="640" height="480"></canvas>
+<script src="./PROJECTNAME.js"></script>
+<script>
+    window.addEventListener("load", async () => {
+        await wasm_bindgen("./PROJECTNAME_bg.wasm.php");
+    });
+</script>
+</body>
+</html>
+HTML;
+
+$indexHtml = str_replace("PROJECTNAME", $projectName, $indexHtml);
+
+$wasmDir = __DIR__ . DIRECTORY_SEPARATOR . "wasm";
+if (!is_dir($wasmDir)) {
+    @mkdir($wasmDir);
+}
+file_put_contents($wasmDir . DIRECTORY_SEPARATOR . "index.html", $indexHtml);
+
+$bgWasmPHP = <<<PHP
+<?php
+
+header("Content-Type: application/wasm");
+readfile(__DIR__ . DIRECTORY_SEPARATOR . pathinfo(__FILE__)["basename"]);
+PHP;
+file_put_contents($wasmDir . DIRECTORY_SEPARATOR . $projectName . "_bg.wasm.php", $bgWasmPHP);
+
+echo <<<TEXT
+
+Build wasm
+    cargo build --release --target wasm32-unknown-unknown
+    wasm-bindgen target/wasm32-unknown-unknown/release/untitled.wasm --out-dir wasm --no-modules --no-typescript
+    
+TEXT;
+
+
+echo "\r\nRun `php -S 0.0.0.0:7788 -t wasm`";
+
+
